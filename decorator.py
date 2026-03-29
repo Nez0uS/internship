a77ec8894207043f83de12c52cc4b6a6a6b9fd5e
Сергей <sergej@MacBook-Pro-Sergej.local>
2026-03-29 19:57:50 +0400

    Add decorators task

---
+++ decorator.py
@@ -0,0 +1,20 @@
+import time
+
+def timing_decorator(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)  # Вызываем функцию и сохраняем результат
+        end = time.perf_counter()
+        print(f"Время выполнения: {end - start:.6f} сек")
+        return result  # Возвращаем результат работы функции
+    return wrapper
+
+
+@timing_decorator
+def slow_function():
+    time.sleep(2)
+
+
+slow_function()
+# 1 task is complate
+
